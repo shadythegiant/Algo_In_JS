21523e029b9a44c49643bb18fdbfa57fd058dfe5
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-05-02 17:03:45 +0100

    revising array methods and learning about time complexity for some of the methods :octocat:

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const instructor = {
 
 instructor.isSingal = true;
 
-console.log(instructor);
+// console.log(instructor);
 
 // deleting
 
 delete instructor.isSingal;
 
-console.log(instructor);
+// console.log(instructor);
 
 // looping
 
@@ -23,14 +23,33 @@ console.log(instructor);
 
 function searchObj(obj, searchValue) {
   const entries = Object.entries(obj);
+
   const keyValueArray = entries.filter((entry) => entry[1] === searchValue);
+
+  if (!keyValueArray.length)
+    return console.log("the value doesn't exist in the obj");
 
   const [key, value] = [...keyValueArray[0]];
 
-  if (!keyValueArray.length) console.log("the value doesn't exist in the obj");
   if (keyValueArray.length > 0)
     console.log(` the value ${value} is stored in the key ${key}`);
 }
 
 searchObj(instructor, "samad");
-console.log(instructor);
+searchObj(instructor, "hey");
+
+// writing the same function for arrays
+
+function searchArr(arr, searchValue) {
+  if (arr.length < 0) return false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].trim() === searchValue.trim()) return true;
+  }
+
+  return `${searchValue} is absent unfortunately ): `;
+}
+
+const array = ["hey", "there", "im trying", 1];
+
+console.log(array.sort((a, b) => (a < b ? 1 : -1)));
